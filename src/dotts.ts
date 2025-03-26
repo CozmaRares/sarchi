b75d3e38f8a6622fb6d6755e1db3fca54efc155a
Cozma Rares <cozmarares11@gmail.com>
2025-03-26 18:50:13 +0200

    change bangs to dotts, match all possible dotts and use first one, add categories to dotts

--- src/dotts.ts
+++ src/dotts.ts
@@ -1,82 +1,111 @@
-const bangs: Readonly<Record<string, { n: string; u: string; k?: boolean }>> = {
+const dotts: Readonly<
+  Record<
+    string,
+    { n: string; u: string; c: string; k?: boolean }
+  >
+> = {
   g: {
     n: "Google",
     u: "https://www.google.com/search?q=%s",
+    c: "Search",
   },
   gi: {
     n: "Google Images",
     u: "https://google.com/search?tbm=isch&q=%s&tbs=imgo:1",
+    c: "Search",
   },
   t3: {
     n: "T3 Chat",
     u: "https://www.t3.chat/new?q=%s",
+    c: "AI",
   },
   fas: {
     n: "Font Awesome",
     u: "https://fontawesome.com/search?q=%s",
+    c: "Icons",
   },
   lu: {
     n: "Lucide",
     u: "https://lucide.dev/icons/?search=%s",
+    c: "Icons",
   },
   npm: {
     n: "NPM",
     u: "https://www.npmjs.com/search?q=%s",
+    c: "Code",
   },
   rs: {
     n: "Docs.rs Crates",
     u: "https://docs.rs/%s",
     k: true,
+    c: "Code",
   },
   rss: {
     n: "Docs.rs",
     u: "https://docs.rs/releases/search?query=%s",
+    c: "Code",
   },
   gh: {
     n: "GitHub Repo",
     u: "https://github.com/%s",
     k: true,
+    c: "Code",
   },
   ghs: {
-    n: "GitHub",
+    n: "GitHub Search",
     u: "https://github.com/search?q=%s",
+    c: "Code",
   },
   tr: {
     n: "Google Translate (auto to English)",
     u: "https://translate.google.com/?sl=auto&tl=en&text=%s&op=translate",
+    c: "Miscellaneous",
   },
   ter: {
     n: "Google Translate (English to Romanian)",
     u: "https://translate.google.com/?sl=en&tl=ro&text=%s&op=translate",
+    c: "Miscellaneous",
   },
   tre: {
     n: "Google Translate (Romanian to English)",
     u: "https://translate.google.com/?sl=ro&tl=en&text=%s&op=translate",
+    c: "Miscellaneous",
   },
   y: {
     n: "YouTube",
     u: "https://www.youtube.com/results?search_query=%s",
+    c: "Search",
   },
   ac: {
     n: "AC UTCN",
     u: "https://ac.utcluj.ro/%s.html",
     k: true,
+    c: "Miscellaneous",
   },
   mal: {
     n: "My Anime List",
     u: "https://myanimelist.net/anime.php?q=%s",
+    c: "Miscellaneous",
   },
   can: {
     n: "Can I Use",
     u: "https://caniuse.com/?search=%s",
+    c: "Code",
   },
   z: {
     n: "Z Library",
     u: "https://z-library.rs/s/?q=%s",
+    c: "Search",
   },
   ctp: {
     n: "CTP Cluj",
     u: "https://ctpcj.ro/index.php/ro/orare-linii/linii-urbane/linia-%s",
+    c: "Miscellaneous",
+  },
+  mdn: {
+    n: "MDN Web Docs",
+    u: "https://developer.mozilla.org/search?q=%s",
+    c: "Code",
   },
 };
-export default bangs;
+export default dotts;
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,37 @@
-import bangs from "./bangs";
+import dotts from "./dotts";
 import "./global.css";
 
 function showHomePage() {
   document.getElementById("app")!.innerHTML = `
-    <div class="content-container" style="height: 100vh; text-align: center">
+    <div class="content-container" style="height: 100svh; text-align: center">
       <div>
         <h1>Sărchi</h1>
         <p>
           <a href="https://duckduckgo.com/">DuckDuckGo</a>'s bang redirects are
           cool. I took <a href="https://x.com/theo">Theo</a>'s client side remake
-          (<a href="https://unduck.link/">und*uck</a>) and made it work with my own bangs.
+          (<a href="https://unduck.link/">und*uck</a>) and made it work with my own dotts.
         </p>
       </div>
-      <div>
+      <div class="home-page-container">
         <p>Add this url as a custom search engine in your browser:</p>
-        <div id="container">
+        <div id="url-container">
           <p id="url">https://sarchi.raru.dev/?q=%s</p>
           <button id="copy">
             <img src="/clipboard.svg" alt="Copy" />
           </button>
         </div>
       </div>
-      <a href="/bangs">
-        See available bangs
+      <div class="home-page-container">
+        <p>Do a search to have the url be automatically picked up by your browser as a recently used search engine</p>
+        <form id="search-form">
+          <input id="search-input" placeholder="Search..."/>
+          <button type="submit">
+            <img src="/clipboard.svg" alt="Search" />
+          </button>
+        </form>
+      </div>
+      <a href="/dotts">
+        See available dotts
         <span>↗</span>
       </a>
     </div>
@@ -32,6 +41,9 @@ function showHomePage() {
   const copyIcon = document.querySelector<HTMLImageElement>("#copy > img")!;
   const url = document.getElementById("url")!.innerText;
 
+  const searchForm = document.getElementById("search-form")!;
+  const searchInput = document.getElementById("search-input") as HTMLInputElement;
+
   copy.addEventListener("click", async () => {
     await navigator.clipboard.writeText(url);
     copyIcon.src = "/clipboard-check.svg";
@@ -40,65 +52,127 @@ function showHomePage() {
       copyIcon.src = "/clipboard.svg";
     }, 2000);
   });
+
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const url = new URL(window.location.href);
+    url.searchParams.set("q", searchInput.value);
+    window.location.replace(url);
+  });
 }
 
-function showBangs() {
-  const bangList =
-    Object.entries(bangs)
-      .map(
-        ([key, value]) =>
-          `<li>
-              <div>.${key}</div>
-              <div>Name: ${value.n}</div>
-              <div style="word-break: break-all;">
-                URL: <span class="font-mono">${value.u}</span>
-              </div>
-              ${!!value.k ? `<div>Keeps slashes in path</div>` : ""}
-          </li>`,
-      )
-      .join("")
+function showDotts() {
+  const dottList = Object.entries(dotts).reduce(
+    (acc: { [key: string]: string }, [key, value]) => {
+      const listItem = `
+        <li>
+          <div>.${key}</div>
+          <div>Name: ${value.n}</div>
+          <div style="word-break: break-all;">
+            URL: <span class="font-mono">${value.u}</span>
+          </div>
+          ${!!value.k ? `<div>Keeps slashes in path</div>` : ""}
+        </li>
+      `;
+
+      if (!acc[value.c]) {
+        acc[value.c] = "";
+      }
+      acc[value.c] += listItem;
+      return acc;
+    },
+    {},
+  );
+
+  const categoryList = Object.entries(dottList)
+    .map(
+      ([category, items]) => `
+      <div>
+        <h2>${category}</h2>
+        <ul class="dott-list">
+          ${items}
+        </ul>
+      </div>
+    `,
+    )
+    .join("");
 
   document.getElementById("app")!.innerHTML = `
      <div class="content-container">
-       <h1>Bangs List</h1>
-        <ul class="bang-list">
-          ${bangList}
-        </ul>
+       <h1>Dott List</h1>
+        ${categoryList}
      </div>
   `;
+
+  //const dottList = Object.entries(dotts)
+  //  .map(
+  //    ([key, value]) => `
+  //      <li>
+  //        <div>.${key}</div>
+  //        <div>Name: ${value.name}</div>
+  //        <div style="word-break: break-all;">
+  //          URL: <span class="font-mono">${value.url}</span>
+  //        </div>
+  //        ${!!value.keepSlashes ? `<div>Keeps slashes in path</div>` : ""}
+  //      </li>
+  //    `,
+  //  )
+  //  .join("");
+  //
+  //document.getElementById("app")!.innerHTML = `
+  //   <div class="content-container">
+  //     <h1>Dott List</h1>
+  //      <ul class="dott-list">
+  //        ${dottList}
+  //      </ul>
+  //   </div>
+  //`;
+}
+
+function useDott({ dott, query }: { dott: string; query: string }) {
+  if (!(dott in dotts)) return false;
+  const selectedDott = dotts[dott];
+
+  query = query.replace("." + dott, "").trim();
+  query = encodeURIComponent(query);
+
+  // keep slashes
+  if (selectedDott.k === true) query = query.replace(/%2F/g, "/");
+
+  const searchUrl = selectedDott.u.replace("%s", query);
+  window.location.replace(searchUrl);
+  return true;
 }
 
 function redirect() {
-  const DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
+  const DEFAULT_DOTT = "g";
 
   const url = new URL(window.location.href);
 
-  if (url.pathname === "/bangs") return showBangs();
+  if (url.pathname === "/dotts") return showDotts();
   if (url.pathname !== "/") return;
 
   let query = url.searchParams.get("q")?.trim() ?? "";
   if (!query) return showHomePage();
 
-  // get first bang
-  const candidate = query.match(/\.(\S+)/i)?.[1]?.toLowerCase();
+  // get first dott
+  const candidates = query.matchAll(/\.(\S+)/gi);
 
-  const bang = (
-    candidate && candidate in bangs ? candidate : DEFAULT_BANG
-  ) as keyof typeof bangs;
-  const selectedBang = bangs[bang];
+  // keep trying for each candidate
+  for (const candidate of candidates)
+    if (
+      useDott({
+        dott: candidate[1].toLowerCase(),
+        query,
+      })
+    )
+      return;
 
-  // remove bang
-  query = query.replace(/\.\S+\s*/i, "").trim();
-  query = encodeURIComponent(query);
-
-  // keep slashes
-  if (selectedBang.k === true) {
-    query = query.replace(/%2F/g, "/");
-  }
-
-  const searchUrl = selectedBang.u.replace("%s", query);
-
-  window.location.replace(searchUrl);
+  // use default
+  useDott({
+    dott: DEFAULT_DOTT,
+    query,
+  });
 }
 
 redirect();
